b7448673ddf749de59774d81664b380657f40a2b
therafigh <therafigh@gmail.com>
2024-05-16 22:00:37 +0300

    Password obscure changer button added

--- torun/lib/main.dart
+++ torun/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: camel_case_types, unnecessary_breaks, library_private_types_in_public_api, use_build_context_synchronously
+// ignore_for_file: camel_case_types, unnecessary_breaks, library_private_types_in_public_api, use_build_context_synchronously, dead_code
 
 import 'package:flutter/material.dart';
 
@@ -6,60 +6,35 @@ String username = '';
 String password = '';
 void main() {
   runApp(
-    MaterialApp(
+     const MaterialApp(
       home: loginPage(),
       debugShowCheckedModeBanner: false,
     ),
   );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: SizedBox(
-            width: 400.0,
-            height: 400.0,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => loginPage()),
-                );
-              },
-              style: const ButtonStyle(
-                backgroundColor:
-                    MaterialStatePropertyAll<Color>(Color(0xFFDDF7E3)),
-              ),
-              child: const Text(
-                'giris',
-                style: TextStyle(fontSize: 50.0, color: Color(0xFFDF2E38)),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class loginPage extends StatefulWidget {
+  const loginPage({super.key});
+
   @override
   loginPageState createState() => loginPageState();
 }
 
 class loginPageState extends State {
+   bool passwordVisible=false; 
+      
+   @override 
+    void initState(){ 
+      super.initState(); 
+      passwordVisible=true; 
+    } 
   // Fonksiyon: Kullanıcıyı giriş yapmaya yönlendirir
   void _login() {
     if (username == '25297638553' && password == 'queue38') {
       // Kullanıcı adı ve şifre doğruysa, müşteri ekranına yönlendir
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (page) => Placeholder()),
+        MaterialPageRoute(builder: (page) => const Placeholder()),
       );
     } else {
       // Kullanıcı adı veya şifre yanlışsa hata göster
@@ -67,11 +42,11 @@ class loginPageState extends State {
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text('Hata'),
-            content: Text('T.C. Kimlik Numarası veya şifre yanlış.'),
+            title: const Text('Hata'),
+            content: const Text('T.C. Kimlik Numarası veya şifre yanlış.'),
             actions: <Widget>[
               TextButton(
-                child: Text('Tamam'),
+                child: const Text('Tamam'),
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
@@ -86,12 +61,9 @@ class loginPageState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFDDF7E3),
-      appBar: AppBar(
-        title: Text('Giriş Ekranı'),
-      ),
+      backgroundColor: const Color(0xFFDDF7E3),
       body: Padding(
-        padding: EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -101,26 +73,39 @@ class loginPageState extends State {
                   username = value;
                 });
               },
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'T.C. Kimlik No.',
               ),
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             TextField(
               onChanged: (value) {
                 setState(() {
                   password = value;
                 });
               },
-              obscureText: true,
-              decoration: InputDecoration(
+              obscuringCharacter: '●',
+              obscureText: passwordVisible,
+              decoration:  InputDecoration(
                 labelText: 'Şifre',
+                 suffixIcon: IconButton( 
+                      icon: Icon(passwordVisible 
+                          ? Icons.visibility 
+                          : Icons.visibility_off), 
+                      onPressed: () { 
+                        setState( 
+                          () { 
+                            passwordVisible = !passwordVisible; 
+                          }, 
+                        ); 
+                      }, 
+                    ),
               ),
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: _login,
-              child: Text('Giriş Yap'),
+              child: const Text('Giriş Yap'),
             ),
           ],
         ),
