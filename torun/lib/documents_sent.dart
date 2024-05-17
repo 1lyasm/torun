7781431ee8af15bb58e9e046cb8c0979f3b68e21
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-05-17 13:56:10 +0300

    Add documents sent page

---
+++ torun/lib/documents_sent.dart
@@ -0,0 +1,78 @@
+// ignore_for_file: unnecessary_import
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DocumentsSentPage extends StatefulWidget {
+  const DocumentsSentPage({super.key});
+
+  @override
+  DocumentsSentPageState createState() => DocumentsSentPageState();
+}
+
+class DocumentsSentPageState extends State {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xFFDDF7E3),
+        body: Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 33.0, top: 150.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child:
+                      Text("Hesap Doğrulama", style: TextStyle(fontSize: 26)),
+                ),
+                SizedBox(width: 16),
+                Padding(
+                  padding: const EdgeInsets.only(right: 33.0),
+                  child: Image.asset(
+                    "assets/trackbar_2.png",
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 300,
+          ),
+          Text(
+            "Belgeleriniz",
+            style: TextStyle(fontSize: 50),
+          ),
+          Text(
+            "Gönderilmiştir",
+            style: TextStyle(fontSize: 50),
+          ),
+          Container(padding: const EdgeInsets.only(top: 20), child: Text("Sonraki İşlem İçin Kimlik belgenizi", style: TextStyle(fontSize: 25),)),
+          Text("Hazırlayınız", style: TextStyle(fontSize: 25),),
+          SizedBox(height: 150),
+          Container(
+            width: 400,
+            height: 90,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(
+                      167, 201, 87, 1), // Set the background color
+                  foregroundColor: Colors.white, // Set the text color
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
+              child: const Text(
+                "Devam Et",
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          ),
+        ]));
+  }
+}
