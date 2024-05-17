2a2531cf15e9bc93f7504dfd4724ed62c0cc7025
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-05-17 04:04:33 +0300

    Add document scan page

---
+++ torun/lib/document_scan.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DocumentScanPage extends StatefulWidget {
+  const DocumentScanPage({super.key});
+
+  @override
+  DocumentScanPageState createState() => DocumentScanPageState();
+}
+
+class DocumentScanPageState extends State {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFDDF7E3),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 33.0, top: 150.0),
+            child: Row(children: [
+              Text("Hesap Doğrulama", style: TextStyle(fontSize: 26))
+            ]),
+          ),
+          SizedBox(
+            height: 200,
+          ),
+          Text("Kimlik Belgesi Taratma", style: TextStyle(fontSize: 26))
+        ],
+      ),
+    );
+  }
+}
