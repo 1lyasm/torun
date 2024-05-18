bc6471f9d1569fb4d681b1a7b2ce1b3dcd88f9fc
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-05-18 10:42:30 +0300

    Fix styling

--- torun/lib/expanded_user_profile_card.dart
+++ torun/lib/expanded_user_profile_card.dart
@@ -40,80 +40,18 @@ class _ExpandedUserProfileCardState extends State<ExpandedUserProfileCard> {
         controlAffinity: ListTileControlAffinity.platform,
         backgroundColor: Colors.grey.shade400,
         title: Padding(
-          padding: const EdgeInsets.all(50.0),
+          padding: const EdgeInsets.all(20),
           child: Container(
             alignment: Alignment.center,
             height: 200,
             width: 425,
             decoration: BoxDecoration(
                 color: Colors.grey.shade400, borderRadius: BorderRadius.circular(20)),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                  side: const BorderSide(color: Color.fromARGB(255, 163, 162, 162), width: 1),
-                  borderRadius: BorderRadius.circular(20)),
-              color: Colors.grey.shade300,
-              elevation: 4.0,
-              margin: const EdgeInsets.all(30.0),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      '${widget.name} ${widget.surname}',
-                      style: const TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 8.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(Icons.location_on_outlined),
-                        const SizedBox(width: 4.0),
-                        Text(widget.address),
-                      ],
-                    ),
-                    const SizedBox(height: 8.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(5, (index) {
-                        return Icon(
-                          index < widget.rating
-                              ? Icons.star
-                              : Icons.star_border,
-                          color: Colors.amber,
-                        );
-                      }),
-                    ),
-                    const SizedBox(height: 4.0),
-                    // Text(
-                    //   '$voteCount',
-                    //   style: TextStyle(
-                    //     fontSize: 18.0,
-                    //     fontWeight: FontWeight.bold,
-                    //   ),
-                    // ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(50.0),
             child: Container(
-              alignment: Alignment.center,
-              height: 400,
-              width: 425,
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade400, borderRadius: BorderRadius.circular(20)),
+              width: 400,
               child: Card(
                 shape: RoundedRectangleBorder(
-                    side: const BorderSide(color: Colors.black, width: 1),
+                    side: const BorderSide(color: Color.fromARGB(255, 163, 162, 162), width: 1),
                     borderRadius: BorderRadius.circular(20)),
                 color: Colors.grey.shade300,
                 elevation: 4.0,
@@ -125,34 +63,105 @@ class _ExpandedUserProfileCardState extends State<ExpandedUserProfileCard> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Text(
-                        'Uygun Saat Aralığı:${widget.from} - ${widget.to}',
+                        '${widget.name} ${widget.surname}',
                         style: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Çalışma Süresi:${widget.workHours} saat',
-                        style: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Görev Kategori:${widget.missions}',
-                        style: const TextStyle(
-                          fontSize: 20.0,
+                          fontSize: 28.0,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                       const SizedBox(height: 8.0),
-                      const ElevatedButton(
-                          onPressed: null,
-                          child: Text(
-                            "Talep gönder",
-                            style: TextStyle(fontSize: 20),
-                          ))
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(Icons.location_on_outlined),
+                          const SizedBox(width: 4.0),
+                          Text(widget.address, style: TextStyle(fontSize: 20),),
+                        ],
+                      ),
+                      const SizedBox(height: 8.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(5, (index) {
+                          return Icon(
+                            index < widget.rating
+                                ? Icons.star
+                                : Icons.star_border,
+                            color: Colors.amber,
+                          );
+                        }),
+                      ),
+                      const SizedBox(height: 4.0),
+                      // Text(
+                      //   '$voteCount',
+                      //   style: TextStyle(
+                      //     fontSize: 18.0,
+                      //     fontWeight: FontWeight.bold,
+                      //   ),
+                      // ),
                     ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 75),
+            child: Container(
+              alignment: Alignment.center,
+              height: 250,
+              width: 425,
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(189, 189, 189, 1), borderRadius: BorderRadius.circular(20)),
+              child: Container(
+                width: 400,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Color.fromRGBO(189, 189, 189, 1), width: 1),
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Colors.grey.shade300,
+                  elevation: 4.0,
+                  margin: const EdgeInsets.all(0.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Uygun Saat Aralığı:    ${widget.from} - ${widget.to}',
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Çalışma Süresi:          ${widget.workHours} saat',
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Görev Kategori:          ${widget.missions}',
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 40.0),
+                        Container(
+                          alignment: Alignment.center,
+                          child: const ElevatedButton(
+                              onPressed: null,
+                              child: Text(
+                                "Talep gönder",
+                                style: TextStyle(fontSize: 28),
+                              )),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
