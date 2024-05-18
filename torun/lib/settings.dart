22ed01bd003f49fe635fe770ac082793b90c4024
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-05-18 10:56:35 +0300

    Improve UI of settings page

--- torun/lib/settings.dart
+++ torun/lib/settings.dart
@@ -59,80 +59,90 @@ class SettingsPageState extends State {
                 fontWeight: FontWeight.bold),
           ),
           const SizedBox(
-              width: 350,
+              width: 175,
               child: Divider(
                 color: Color.fromRGBO(167, 201, 87, 1),
                 thickness: 5,
               )),
-          const Card(
-            color: Color(0xFFE9E9E9),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      'DOĞUM TARİHİ',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFF086634),
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'NUMARA',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFF086634),
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'E-POSTA',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFF086634),
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'ADRES',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFF086634),
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      '19.05.1962',
-                      style: TextStyle(
-                        fontSize: 20,
+          const SizedBox(
+            height: 50,
+          ),
+          Container(
+            width: 400,
+            height: 200,
+            child: const Card(
+              color: Color.fromRGBO(238, 238, 238, 1),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'DOĞUM TARİHİ',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF086634),
+                            fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    Text(
-                      '+903789246066',
-                      style: TextStyle(
-                        fontSize: 20,
+                      Text(
+                        'NUMARA',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF086634),
+                            fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    Text(
-                      'rafiqog@bk.ru',
-                      style: TextStyle(
-                        fontSize: 20,
+                      Text(
+                        'E-POSTA',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF086634),
+                            fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    Text(
-                      'Istanbul, Beşiktaş',
-                      style: TextStyle(
-                        fontSize: 20,
+                      Text(
+                        'ADRES',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF086634),
+                            fontWeight: FontWeight.bold),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        '19.05.1962',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        '+903789246066',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        'rafiqog@bk.ru',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        'Istanbul, Beşiktaş',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
+          ),
+          const SizedBox(
+            height: 50,
           ),
           const SizedBox(
               width: 350,
@@ -140,23 +150,26 @@ class SettingsPageState extends State {
                 color: Color.fromRGBO(167, 201, 87, 1),
                 thickness: 5,
               )),
+          const SizedBox(
+            height: 50,
+          ),
           const Text(
             'Geçmiş işlerim',
-            style: TextStyle(
-              color: Color(0xFFA7C957),
-            ),
+            style: TextStyle(color: Color(0xFFA7C957), fontSize: 30),
+          ),
+          const SizedBox(
+            height: 10,
           ),
           const Text(
             'Gizlilik Politikası',
-            style: TextStyle(
-              color: Color(0xFFA7C957),
-            ),
+            style: TextStyle(color: Color(0xFFA7C957), fontSize: 30),
+          ),
+          const SizedBox(
+            height: 10,
           ),
           const Text(
             'Hakkımızda',
-            style: TextStyle(
-              color: Color(0xFFA7C957),
-            ),
+            style: TextStyle(color: Color(0xFFA7C957), fontSize: 30),
           ),
         ],
       ),
